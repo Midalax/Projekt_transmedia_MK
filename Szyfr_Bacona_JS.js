3f828b2ecb93b675ec38d2cff4b124dbdbc67bf5
Midalax <65863973+Midalax@users.noreply.github.com>
2025-01-06 23:50:24 +0100

    +

--- Szyfr_Bacona_JS.js
+++ Szyfr_Bacona_JS.js
@@ -1,13 +1,13 @@
 function checkAnswer() {
-    const correctAnswer = "szyfr polibiusza";
-    const correctAnswer_nospace = "szyfrpolibiusza";
-    const correctAnswer_pause = "szyfr-polibiusza"; // Oczekiwana odpowiedź
+    const correctAnswer = "szyfr bacona";
+    const correctAnswer_nospace = "szyfrbacona";
+    const correctAnswer_pause = "szyfr-bacona"; // Oczekiwana odpowiedź
     const userAnswer = document.getElementById('solution').value.trim();
     const feedback = document.getElementById('feedback');
     const rightSectionText = document.getElementById('right-section-text'); // Pobranie diva
 
     if (userAnswer.toLowerCase() === correctAnswer.toLowerCase() || userAnswer.toLowerCase() === correctAnswer_nospace.toLowerCase() || userAnswer.toLowerCase() === correctAnswer_pause.toLowerCase()  ) {
-        feedback.textContent = "Udało ci się rozwiązać szyfr cezara! (2 = H4)";
+        feedback.textContent = "Udało ci się rozwiązać szyfr cezara! (3 = K6)";
         feedback.style.color = "green";
 
         rightSectionText.style.display="block";
